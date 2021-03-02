de433f7a708c7e3c26182b83acf6d3c7ff903659
Vichichi <aichhorn.vicky@gmail.com>
2021-03-02 09:07:54 +0100

    Project initialized

---
+++ Assets/Scenes/changeColor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeColor : MonoBehaviour
+{
+    private int Randomizer;
+
+    public Image colorImage;
+
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            Randomizer = Random.Range(0, 4);
+
+            Debug.Log(Randomizer);
+
+            if(Randomizer == 0)
+            {   
+                colorImage.color = new Color32(255,205,25,100);
+            }
+            
+            if(Randomizer == 1)
+            {
+                colorImage.color = new Color32(95,34,0,100);
+            }
+
+            if(Randomizer == 2)
+            {
+                colorImage.color = new Color32(207,63,21,100);
+            }
+
+            if(Randomizer == 3)
+            {
+                colorImage.color = new Color32(102,47,84,100);
+            }
+        }
+    }
+}
+  
